f6867ff0ed4e1b8c72773f22f063d4154e17d053
Bob Loblaw <iwrite@selfdocumentingco.de>
2025-09-13 23:36:47 +0200

    Add postgres-dev

---
+++ postgres-dev/postgres-dev-init.sql
@@ -0,0 +1,5 @@
+CREATE USER "kattbot" WITH PASSWORD 'hunter2';
+CREATE USER "nellebot" WITH PASSWORD 'hunter2';
+
+CREATE DATABASE "kattbot-dev" WITH OWNER "kattbot";
+CREATE DATABASE "nellebot-dev" WITH OWNER "nellebot";
